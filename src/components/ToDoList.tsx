821f390db56357757d56558ce597afab102ac6a5
Manpreet Singh Lalria <manpreet.singh.lalria@gmail.com>
2025-03-12 07:01:28 +0000

    feat: add an example to do list display

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,7 +1,31 @@
 const ToDoList = () => {
+  const toDos = [
+    {
+      id: 1,
+      text: 'Create a to do list',
+      complete: true,
+    },
+    {
+      id: 2,
+      text: 'Do some Simran',
+      complete: false,
+    },
+  ];
   return (
     <div>
       <h1 className='text-3xl font-bold'>To Do List</h1>
+      <ul>
+        {toDos.map((toDo) => (
+          <li>
+            <input
+              type='checkbox'
+              id={toDo.id.toString()}
+              name={toDo.id.toString()}
+            />
+            <label htmlFor='vehicle1'>{toDo.text}</label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
